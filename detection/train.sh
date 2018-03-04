6e353846e93052a7638ce22ef64ccd505deea4af
root <guotongxue123@163.com>
2018-03-04 06:12:06 -0800

    facedetect

---
+++ detection/train.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+/opt/caffe/build/tools/caffe train --solver=/root/Desktop/face/solver.prototxt #\
+#--weights=models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
+#--gpu 0 
